cfada48ea97002c50aabeab0167fc634f14b7317
yvma0 <116330779+yvma0@users.noreply.github.com>
2023-05-09 23:20:08 -0700

    starting js

---
+++ main.js
@@ -0,0 +1,5 @@
+// grab daily, weekly, monthly as separate elements probably
+// set onclick events for them...apply an active state or just a style? TBD
+// grab..the p tags in each section for changing
+// there should be a way to grab it as an array and feed the json data in through array...? tbc thought
+// maybe also try to figure out isolated hover states in js? when hovering over the svgs...
