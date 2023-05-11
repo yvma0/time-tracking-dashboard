a1c1c897e0d36014fdb529da982680aab183ffc7
yvma0 <116330779+yvma0@users.noreply.github.com>
2023-05-10 19:43:38 -0700

    fetch buttons, add event listeners, further setup

--- js/main.js
+++ js/main.js
@@ -4,4 +4,21 @@
 // there should be a way to grab it as an array and feed the json data in through array...? tbc thought
 // maybe also try to figure out isolated hover states in js? when hovering over the svgs...
 
-console.log(data);
+/*===FETCH CARD TEXT TO BE CHANGED===*/
+const text = document.querySelectorAll('p.center-txt');
+// console.log(text);
+// console.log(text[1]);
+// console.log(text[1].textContent);
+
+/*===FETCH BUTTONS===*/
+//=> NOTE: this selects all 'li's; may mess things up in later (hypothetical) feature
+//   additions if any more li items are added <=
+const nav = document.querySelectorAll('li');
+const dailyBtn = nav[0];
+const weeklyBtn = nav[1];
+const monthlyBtn = nav[2];
+
+/*===LISTEN FOR CLICKS===*/
+dailyBtn.addEventListener('click', functionPlaceholder);
+weeklyBtn.addEventListener('click', functionPlaceholder);
+monthlyBtn.addEventListener('click', functionPlaceholder);
