83073d6cb610672000504a36b9707d1e54d0e80f
yvma0 <116330779+yvma0@users.noreply.github.com>
2023-05-10 17:10:24 -0700

    changed json to js, modified imports

--- js/main.js
+++ js/main.js
@@ -3,3 +3,5 @@
 // grab..the p tags in each section for changing
 // there should be a way to grab it as an array and feed the json data in through array...? tbc thought
 // maybe also try to figure out isolated hover states in js? when hovering over the svgs...
+
+console.log(data);
